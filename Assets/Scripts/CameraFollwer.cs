9bb544b613babb925cd787830dd30093968b2530
Poincare <iampengkeyao@gmail.com>
2020-02-03 23:51:27 +0100

    A FPS demo
    My demo of FPS by unity, done in 2017

---
+++ Assets/Scripts/CameraFollwer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollwer : MonoBehaviour {
+    public Transform target;
+    public float smoothing=5;
+    Vector3 offset;
+    internal static object main;
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - target.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 camTarget = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, camTarget, smoothing * Time.deltaTime);
+		
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed=6.0f;
+    public float rayDistence = 100.0f;
+   // Rigidbody rg;
+    Animator ani;
+    Vector3 movement;
+    public LayerMask mask;
+    void Awake()
+    {
+       // rg= GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Move(h, v);
+        Turn();
+        Animating(h * h + v * v);
+    }
+    private void Move(float h,float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        transform.Translate(movement, Space.World);
+    }
+    void Turn()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray,out hit, rayDistence, mask))
+        {
+            Vector3 toMouse = hit.point - transform.position;
+            toMouse.y = 0;
+            transform.rotation = Quaternion.LookRotation(toMouse);
+        }
+    }
+    void Animating(float x)
+    {
+        bool animate = (x != 0);
+        ani.SetBool("IsWalking", animate);
+    }
+
+}
